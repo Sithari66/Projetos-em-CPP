1e5342043b2282ba7ce55e0527bc1fbcc3579f24
Sithari66 <99299281+Sithari66@users.noreply.github.com>
2022-02-14 21:56:41 -0300

    Create conversormspkmh.cpp

---
+++ conversormspkmh.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+float ms;
+
+cout<<"\tConversor de M/s para KM/h";
+cout<<"\nDigite o valor em M/s:";
+cin>>ms;
+
+cout<<"\nO valor em Km/h e igual a ";
+cout<<ms*3.6;
+
+return(0);
+system("pause");
+}
+
