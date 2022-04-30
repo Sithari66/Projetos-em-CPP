eda9436645db419c51d9b26f731a1058a2a447b5
Sithari66 <99299281+Sithari66@users.noreply.github.com>
2022-04-30 18:41:31 -0300

    Create fatorial.cpp
    O fatorial (!) de um número n, representado por n!, é a multiplicação de n por seus antecessores maiores ou iguais a 1. Essa operação é muito comum em análise combinatória.

---
+++ fatorial.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+//Fatorial modularizado
+int fatorial(int n, int fat=1){ 
+    
+    for(fat;n>1;n=n-1){
+        fat=fat*n;
+    }
+    
+   return fat;
+}
+
+int main() {
+  
+  int n;
+  
+  cout<<"\tCalculo Fatorial N!";
+  cout<<"\n\nDigite um numero para ser calculado: ";
+  cin>>n;
+  
+  cout<<n<<" fatorado e igual a "<<fatorial(n);
+
+    return 0;
+}
