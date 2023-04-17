a275709b6c4aa1af3c316fe7cfbcd0964481bc18
Sithari66 <99299281+Sithari66@users.noreply.github.com>
2023-04-17 17:18:29 -0300

    Create igualdadedenumeros.c

---
+++ igualdadedenumeros.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    
+int n1,n2;
+
+printf("Digite 2 numeros: ");
+scanf("%d%d",&n1,&n2);
+
+if(n1==n2)
+    printf("\nOs numeros sao iguais");
+else
+    printf("\nOs numeros nao são iguais");
+
+}
