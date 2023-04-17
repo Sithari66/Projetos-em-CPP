5193447ab04c7c6dc15b40ed085b19ef1bf49184
Sithari66 <99299281+Sithari66@users.noreply.github.com>
2023-04-17 17:19:28 -0300

    Create positivoounegativo.c

---
+++ positivoounegativo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    
+int n;
+
+printf("Digite um numero numero: ");
+scanf("%d",&n);
+
+
+if(n > 0)
+    printf("\nO numero e positivo");
+else
+    printf("\nO numero e negativo");
+
+}
