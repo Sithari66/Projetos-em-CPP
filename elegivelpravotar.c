05c3c3ac322fc67a8665e2100883c0f8fc0cdbb5
Sithari66 <99299281+Sithari66@users.noreply.github.com>
2023-04-17 17:19:56 -0300

    Create elegivelpravotar.c

---
+++ elegivelpravotar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    
+int n;
+
+printf("Digite sua idade: ");
+scanf("%d",&n);
+
+
+if(n>=16)
+    printf("\nParabéns! Você é elegível para votar.");
+else
+    printf("\nDesculpe! Você não é elegível para votar.");
+
+}
