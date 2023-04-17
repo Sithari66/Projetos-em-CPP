0b3c756a9604939061ff4072bad907905a00df0d
Sithari66 <99299281+Sithari66@users.noreply.github.com>
2023-04-17 15:26:04 -0300

    Create contagemregressiva.cpp

---
+++ contagemregressiva.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+
+int main(){
+	
+	int x = 10;
+	
+	while(x>=0){
+		
+	cout<<"\n\n\n\n\n\n\n\n\t\t\t\t\t"<<x<<"\n";
+	x--;
+	Sleep(1000);
+	system("cls");			
+	}
+	
+cout<<"\n\n\n\n\t\t\t\t===Launcher==>\n\n\n\n";
+	
+system("pause");	
+return (0);
+	
+}
