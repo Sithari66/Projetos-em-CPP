8ede9ce7ef6e557ade47c69698a83ffe393ec7fb
Sithari66 <99299281+Sithari66@users.noreply.github.com>
2023-04-17 16:18:34 -0300

    Create conversorfahrenheitpcelsius.cpp

---
+++ conversorfahrenheitpcelsius.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+float F;
+
+cout<<"\t\Conversor de Fahrenheit para Celsius";
+cout<<"\n\nDigite o valor em Fahrenheit :";
+cin>>F;
+
+cout<<"\nO resultado em Celsius e = "<<(F-32)*5/9;
+cout<<"\n\n";
+
+
+system("pause");
+return(0);
+
+}
+
