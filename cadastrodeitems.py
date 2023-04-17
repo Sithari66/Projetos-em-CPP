c2d1e6f22fed6e97a829de5b0b0cad6dac17b29d
Sithari66 <99299281+Sithari66@users.noreply.github.com>
2023-04-17 14:34:29 -0300

    Create cadastrodeitems.py

---
+++ cadastrodeitems.py
@@ -0,0 +1,19 @@
+a = input ("\nDigite o Item 1: ")
+b = input ("Digite o Item 2: ")
+c = input ("Digite o Item 3: ")
+d = input ("Digite o Item 4: ")
+e = input ("Digite o Item 5: ")
+
+
+print("\n")
+num1 = input("Digite a quantidade do item 1: ")
+num2 = input('Digite a quantidade do item 2: ')
+num3 = input('Digite a quantidade do item 3: ')
+num4 = input('Digite a quantidade do item 4: ')
+num5 = input('Digite a quantidade do item 5: ')
+
+print("\nItem 1 - " ''+a+''  " - Quantidade: "  ''+num1+'')
+print("Item 2 - " ''+b+''  " - Quantidade: "  ''+num2+'')
+print("Item 3 - " ''+c+''  " - Quantidade: "  ''+num3+'')
+print("Item 4 - " ''+d+''  " - Quantidade: "  ''+num4+'')
+print("Item 5 - " ''+c+''  " - Quantidade: "  ''+num5+'')
