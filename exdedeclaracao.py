bdcc04d0d5fc8a88793122d76c6dafffd723df8d
Sithari66 <99299281+Sithari66@users.noreply.github.com>
2023-04-17 15:11:27 -0300

    Create exdedeclaracao.py

---
+++ exdedeclaracao.py
@@ -0,0 +1,16 @@
+#Cite três exemplos distintos de declaração de variável em Pyhton.
+
+#String
+
+name = 'Kira'
+print(name)
+
+#Int
+
+number = 10
+print(number)
+
+#Boolean
+
+false = True
+print(false)
