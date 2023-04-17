769744fa1e518f39da2df8e18050f3857d2e5265
Sithari66 <99299281+Sithari66@users.noreply.github.com>
2023-04-17 14:50:31 -0300

    Create tabuada.py

---
+++ tabuada.py
@@ -0,0 +1,15 @@
+#Escrever um algoritmo que leia um valor para uma variável N de 1 a 10 e faça duas funções que calcule a tabuada de N com duas estruturas de repetição distintas. Mostre a tabuada na forma: 0 x N = 0, 1 x N = 1N, 2 x N = 2N, ..., 10 x N = 10N.
+
+n = int (input("Digite um número de 0 a 10: "))
+
+print("\t\t\nO Número digitado para criação da tabuada foi " +str(n))
+print("\nO primeiro resultado e "+str(n*1))
+print("O segundo resultado e "+str(n*2))
+print("O terceiro resultado e "+str(n*3))
+print("O quarto resultado e " +str(n*4))
+print("O quinto resultado e " +str(n*5))
+print("O sexto resultado e " +str(n*6))
+print("O setimo resultado e " +str(n*7))
+print("O oitavo resultado e " +str(n*8))
+print("O nono resultado e " +str(n*9))
+print("O decimo resultado e " +str(n*10))
