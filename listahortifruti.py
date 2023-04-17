de2737a3fa8fcf7b3bd79a154165fc64b6ab8214
Sithari66 <99299281+Sithari66@users.noreply.github.com>
2023-04-17 15:05:20 -0300

    Create listahortifruti.py

---
+++ listahortifruti.py
@@ -0,0 +1,13 @@
+fruta1 = input("Digite a fruta: ")
+fruta2 = input("Digite a fruta: ")
+fruta3 = input("Digite a fruta: ")
+fruta4 = input("Digite a fruta: ")
+fruta5 = input("Digite a fruta: ")
+
+price = 1
+
+print("\nA Fruta " + fruta1 + " custa " ,price)
+print("A Fruta " + fruta2 + " custa " ,price*2)
+print("A Fruta " + fruta3 + " custa " ,price/2)
+print("A Fruta " + fruta4 + " custa " ,price/2*3)
+print("A Fruta " + fruta5 + " custa " ,price/2*3/2)
